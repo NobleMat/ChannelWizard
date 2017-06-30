e8c984c64d8c2fa7d21c6aabc9b7948746ba935c
Noble Mathew <noby.matty@gmail.com>
2017-06-30 10:22:10 +1000

    Initial Project Commit

---
+++ Channel7Test/Channel7Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Channel7Test
+//
+//  Created by Noble Mathew on 30/6/17.
+//  Copyright © 2017 NTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
